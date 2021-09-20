e4ee5ec91c66757730114502385bf509300ef7dc
Mukul Pathania <mukulpathania96@gmail.com>
2021-09-20 18:58:12 +0530

    refactor: Move roomCard into a separate component
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -10,7 +10,7 @@ import { useToast } from 'contexts/ToastContext';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import link from 'link';
-import { formatDistanceToNow } from 'date-fns';
+import RoomCard from 'components/RoomCard';
 
 type StatCardProps = {
   icon: string;
@@ -30,38 +30,6 @@ const StatCard = (props: StatCardProps): JSX.Element => {
         </p>
         <p className="text-center p-2 text-lg text-primary-400 font-medium">
           {props.text}
-        </p>
-      </div>
-    </Link>
-  );
-};
-
-type RoomCardProps = {
-  name: string;
-  created_at: Date;
-  polls: number;
-  members: number;
-  room_id: string;
-};
-const RoomCard = (props: RoomCardProps): JSX.Element => {
-  return (
-    <Link href={{ pathname: link.specificRoom, query: { rid: props.room_id } }}>
-      <div className="cursor-pointer relative px-6 py-12 bg-white max-w-lg rounded hover:shadow-2xl duration-500 transform hover:-translate-y-1">
-        <h3 className="text-3xl text-primary-500 font-semibold pb-2 truncate">
-          {props.name}
-        </h3>
-        <p className="text-lg font-medium text-gray-500">
-          Created{' '}
-          {formatDistanceToNow(new Date(props.created_at), {
-            addSuffix: true,
-            includeSeconds: true,
-          })}
-        </p>
-        <p className="text-lg font-medium text-secondary-800 pt-6">
-          {props.members} members
-        </p>
-        <p className="absolute -top-4 md:-top-4 -right-3 md:-right-10 w-24 h-12 border-2 border-primary-500 bg-gray-200 rounded-full flex justify-center items-center p-2 font-medium text-base">
-          {props.polls} {props.polls === 1 ? 'poll' : 'polls'}
         </p>
       </div>
     </Link>
